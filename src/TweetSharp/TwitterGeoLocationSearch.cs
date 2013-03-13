6b758122dd2bbc04a7382c79d14ae1159369bba6
Ken Sykora <ken.sykora@gmail.com>
2013-03-12 23:14:57 -0500

    Cleaned up documentation

--- src/TweetSharp/TwitterGeoLocationSearch.cs
+++ src/TweetSharp/TwitterGeoLocationSearch.cs
@@ -5,16 +5,31 @@
     /// </summary>
     public class TwitterGeoLocationSearch : TwitterGeoLocation
     {
+        /// <summary>
+        /// Radius type in Miles or Kilometers
+        /// </summary>
         public enum RadiusType
         {
             Mi,
             Km
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TwitterGeoLocationSearch"/> struct.
+        /// </summary>
         public TwitterGeoLocationSearch()
         {
         }
 
+        /// <summary>
+        /// /// <summary>
+        /// Initializes a new instance of the <see cref="TwitterGeoLocation"/> struct.
+        /// </summary>
+        /// </summary>
+        /// <param name="latitutde">The latitude of search location.</param>
+        /// <param name="longitude">The longitude of search location.</param>
+        /// <param name="radius">The radius of search location.</param>
+        /// <param name="unitOfMeasurement">The unit of measurement (Mi or Km).</param>
         public TwitterGeoLocationSearch(double latitutde, double longitude, int radius, RadiusType unitOfMeasurement)
             : base(latitutde, longitude)
         {
@@ -25,7 +40,7 @@
         /// <summary>
         ///     Radius in specified <see cref="RadiusType" />
         /// </summary>
-        /// <seealso cref="" />
+        /// <seealso cref="UnitOfMeasurement" />
         public int Radius { get; set; }
 
         public RadiusType UnitOfMeasurement { get; set; }
