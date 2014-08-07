50d2f5dd28eeeb1cf790e6c0a8d9eacab0fc95a1
Yortw <troywillmot@gmail.com>
2014-08-07 21:25:10 +1200

    Updates to prepare new nuget package
    Moved version # attribute to separate assemblyinfoversion.cs file and
    shared among all projects, so they all build with the same version
    number. Built code. Updated nuget metdata, batch files, rebuilt package.

--- src/TweetSharp.Silverlight/Properties/AssemblyInfo.cs
+++ src/TweetSharp.Silverlight/Properties/AssemblyInfo.cs
@@ -24,15 +24,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e3a252c7-d342-4a04-95a5-e2fbccdccbc5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
