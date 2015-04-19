53624f0efeb97b0e70bad00f665691556defc671
Yortw <troywillmot@gmail.com>
2015-04-19 20:48:29 +1200

    Updates for new release.
    Increase version # in assembly info.
    Update nuspec file.
    Upgrade dependency packages.

--- src/TweetSharp/Properties/AssemblyInfoVersion.cs
+++ src/TweetSharp/Properties/AssemblyInfoVersion.cs
@@ -9,5 +9,5 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 using System.Reflection;
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.1")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
